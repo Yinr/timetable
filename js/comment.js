a7c5df2682b99e7e3377ae91dc2f305f429cd766
Yinr <hssyinr@yeah.net>
2017-02-19 00:00:55 +0800

    add duoshuo comment

---
+++ js/comment.js
@@ -0,0 +1,27 @@
+var vmComment = new Vue({
+    el: '#comment',
+    data: {
+        duoshuo_name: "yinr",
+        key: encodeURI(location.host + location.pathname),
+        title: document.title,
+        image: "http://yinrs.win/favicon.ico",
+        content: document.title,
+        url: encodeURI(location.href)
+    },
+    mounted: function() {
+        // 多说公共JS代码 start (一个网页只需插入一次)
+        window.duoshuoQuery = {
+            short_name: this.duoshuo_name // 多说用户简称
+        };
+        (function() {
+            var ds = document.createElement('script');
+            ds.type = 'text/javascript';
+            ds.async = true;
+            ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+            ds.charset = 'UTF-8';
+            (document.getElementsByTagName('head')[0] ||
+                document.getElementsByTagName('body')[0]).appendChild(ds);
+        })();
+        // 多说公共JS代码 end
+    }
+});
