6e18a9f250f2583ec4e03d01c48224a72b870bc9
Yinr <hssyinr@yeah.net>
2017-02-20 13:50:07 +0800

    update comment key

--- js/comment.js
+++ js/comment.js
@@ -2,7 +2,7 @@ var vmComment = new Vue({
     el: '#comment',
     data: {
         duoshuo_name: "yinr",
-        key: encodeURI(location.host + location.pathname).replace(/\/$/,""),
+        key: document.title + " _ " +encodeURI(location.host + location.pathname).replace(/\/$/,""),
         title: document.title,
         image: "http://yinrs.win/favicon.ico",
         content: document.title,
