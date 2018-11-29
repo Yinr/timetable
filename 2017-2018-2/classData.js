2ad8c24d7fd4f1f73f86b7f71a8b6178ecce2048
Yinr <yinr@yinr.cc>
2018-11-29 20:08:45 +0800

    update 2018-2019-1 (temp)

---
+++ 2017-2018-2/classData.js
@@ -0,0 +1,169 @@
+// 北中医2013级中医学（卓越针灸对外）班（2018年2～6月）
+var classes = {
+    title: "13针八 2017-2018-2 课表",
+    startDate: "2018/2/26",
+    startWeek: 1,
+    weeks: 4,
+    daysInWeek: 4,
+    description: {
+        "选修课": "神经病学18学时、急诊学63学时、中西医结合骨伤18学时、皮肤病学36学时"
+    },
+    links: [{
+            text: "课件",
+            href: "https://pan.baidu.com/s/1xl6dhTYZhEg5zD87J2Q-Mg",
+            info: "百度云，密码: 92aw"
+        }, {
+            text: "课件",
+            href: "https://cloud.yinr.cc/s/pcFp9eCZ3SbgXo3?path=%2F2017-2018-2",
+            info: "备用,YinrCloud"
+        }],
+    classTime: {
+        a: [{ text: "上午", start: "8:30", end: "11:30" }],
+        p: [{ text: "下午", start: "13:30", end: "16:30" }]
+    },
+    place: ["原幼儿园楼3楼教室第二教室"],
+    settings: {
+        showAllWeeks: false,
+        thisWeek: 0,
+        group: null
+    },
+    classes: {
+        "2018/2/27p": {
+        	subject: "神经病学",
+        	detail: "马晓晶"
+        },
+        "2018/2/28p": {
+        	subject: "皮肤病学"
+        },
+        "2018/3/1a": {
+        	subject: "中西医结合骨伤",
+            detail: "腰椎间盘突出症"
+        },
+        "2018/3/1p": {
+        	subject: "急诊学"
+        },
+        "2018/3/5p": {
+        	subject: "急诊学"
+        },
+        "2018/3/6p": {
+        	subject: "神经病学",
+        	detail: "贾宝辉"
+        },
+        "2018/3/7p": {
+        	subject: "皮肤病学"
+        },
+        "2018/3/8a": {
+        	subject: "中西医结合骨伤",
+            detail: "骨关节病"
+        },
+        "2018/3/8p": {
+        	subject: "急诊学"
+        },
+        "2018/3/12p": {
+        	subject: "急诊学"
+        },
+        "2018/3/13p": {
+        	subject: "神经病学",
+        	detail: "徐海蓉"
+        },
+        "2018/3/14p": {
+        	subject: "皮肤病学"
+        },
+        "2018/3/15a": {
+        	subject: "中西医结合骨伤",
+            detail: "股骨头坏死"
+        },
+        "2018/3/15p": {
+        	subject: "急诊学"
+        },
+        "2018/3/19a": {
+        	subject: "中西医结合骨伤"
+        },
+        "2018/3/19p": {
+        	subject: "急诊学"
+        },
+        "2018/3/20p": {
+        	subject: "急诊学"
+        },
+        "2018/3/21a": {
+            subject: "神经病学"
+        },
+        "2018/3/21p": {
+        	subject: "皮肤病学"
+        },
+        "2018/3/22a": {
+        	subject: "中西医结合骨伤",
+            detail: "颈椎"
+        },
+        "2018/3/22p": {
+        	subject: "神经病学",
+        	detail: "张维"
+        },
+        "2018/3/26p": {
+        	subject: "急诊学"
+        },
+        "2018/3/27p": {
+        	subject: "急诊学"
+        },
+        "2018/3/28p": {
+        	subject: "皮肤病学"
+        },
+        "2018/3/29a": {
+        	subject: "中西医结合骨伤",
+            detail: "创伤"
+        },
+        "2018/3/29p": {
+        	subject: "神经病学",
+        	detail: "彭唯娜"
+        },
+        "2018/4/2p": {
+        	subject: "急诊学"
+        },
+        "2018/4/3p": {
+        	subject: "急诊学"
+        },
+        "2018/4/4p": {
+        	subject: "皮肤病学"
+        },
+        "2018/4/9p": {
+        	subject: "急诊学"
+        },
+        "2018/4/10p": {
+        	subject: "神经病学",
+        	detail: "考试"
+        },
+        "2018/4/11p": {
+        	subject: "皮肤病学"
+        },
+        "2018/4/12p": {
+        	subject: "中西医结合骨伤",
+        	detail: "考试"
+        },
+        "2018/4/16p": {
+        	subject: "急诊学"
+        },
+        "2018/4/17p": {
+        	subject: "急诊学"
+        },
+        "2018/4/18p": {
+        	subject: "皮肤病学"
+        },
+        "2018/4/23p": {
+        	subject: "急诊学"
+        },
+        "2018/4/24p": {
+        	subject: "急诊学"
+        },
+        "2018/4/25p": {
+        	subject: "皮肤病学"
+        },
+        "2018/5/9p": {
+        	subject: "皮肤病学",
+        	detail: "考试"
+        },
+        "2018/5/16a": {
+        	subject: "急诊学",
+        	detail: "考试"
+        }
+    }
+};
--- js/classData.js
+++ js/classData.js
@@ -1,169 +1,118 @@
 // 北中医2013级中医学（卓越针灸对外）班（2018年2～6月）
 var classes = {
-    title: "13针八 2017-2018-2 课表",
-    startDate: "2018/2/26",
+    title: "北中医2018级研究生 2018-2019-1 部分课表",
+    startDate: "2018/9/3",
     startWeek: 1,
-    weeks: 4,
-    daysInWeek: 4,
+    weeks: 20,
+    daysInWeek: 7,
     description: {
-        "选修课": "神经病学18学时、急诊学63学时、中西医结合骨伤18学时、皮肤病学36学时"
+        "必修课": "blah",
+        "选修课": "头面部局部解剖学 and so on"
     },
     links: [{
-            text: "课件",
-            href: "https://pan.baidu.com/s/1xl6dhTYZhEg5zD87J2Q-Mg",
-            info: "百度云，密码: 92aw"
-        }, {
-            text: "课件",
-            href: "https://cloud.yinr.cc/s/pcFp9eCZ3SbgXo3?path=%2F2017-2018-2",
-            info: "备用,YinrCloud"
-        }],
+        text: "选课系统",
+        href: "http://bucmyjs.yinr.cc",
+        info: ""
+    }],
     classTime: {
         a: [{ text: "上午", start: "8:30", end: "11:30" }],
-        p: [{ text: "下午", start: "13:30", end: "16:30" }]
+        p: [{ text: "下午", start: "13:30", end: "16:30" }],
+        e: [{ text: "晚上", start: "18:00", end: "21:10"}]
     },
-    place: ["原幼儿园楼3楼教室第二教室"],
+    place: ["东校区", "西校区"],
     settings: {
         showAllWeeks: false,
         thisWeek: 0,
         group: null
     },
     classes: {
-        "2018/2/27p": {
-        	subject: "神经病学",
-        	detail: "马晓晶"
+        "2018/11/30a": {
+        	subject: "头面部局部解剖学",
+            detail: "西-解剖三"
         },
-        "2018/2/28p": {
-        	subject: "皮肤病学"
+        "2018/11/29e": {
+        	subject: "临床实践方法学讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/1a": {
-        	subject: "中西医结合骨伤",
-            detail: "腰椎间盘突出症"
+        "2018/12/6e": {
+        	subject: "临床实践方法学讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/1p": {
-        	subject: "急诊学"
+        "2018/12/13e": {
+        	subject: "临床实践方法学讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/5p": {
-        	subject: "急诊学"
+        "2018/12/20e": {
+        	subject: "临床实践方法学讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/6p": {
-        	subject: "神经病学",
-        	detail: "贾宝辉"
+        "2018/12/27e": {
+        	subject: "临床实践方法学讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/7p": {
-        	subject: "皮肤病学"
+        "2018/11/30e": {
+        	subject: "传统文化讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/8a": {
-        	subject: "中西医结合骨伤",
-            detail: "骨关节病"
+        "2018/12/7e": {
+        	subject: "传统文化讲座D班(自)",
+            detail: "东-9教"
         },
-        "2018/3/8p": {
-        	subject: "急诊学"
+        "2018/12/14e": {
+        	subject: "传统文化讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/12p": {
-        	subject: "急诊学"
+        "2018/12/21e": {
+        	subject: "传统文化讲座D班",
+            detail: "东-9教"
         },
-        "2018/3/13p": {
-        	subject: "神经病学",
-        	detail: "徐海蓉"
+        "2018/12/1e": {
+        	subject: "中医经典导读（内径A班）",
+            detail: "东-10教"
         },
-        "2018/3/14p": {
-        	subject: "皮肤病学"
+        "2018/12/8e": {
+        	subject: "中医经典导读（内径A班）",
+            detail: "东-10教"
         },
-        "2018/3/15a": {
-        	subject: "中西医结合骨伤",
-            detail: "股骨头坏死"
+        "2018/12/15e": {
+        	subject: "中医经典导读（内径A班）",
+            detail: "东-10教"
         },
-        "2018/3/15p": {
-        	subject: "急诊学"
+        "2018/12/22e": {
+        	subject: "中医经典导读（内径A班）",
+            detail: "东-10教"
         },
-        "2018/3/19a": {
-        	subject: "中西医结合骨伤"
+        "2018/12/2a": {
+        	subject: "人文医学职业技能培训课程D班",
+            detail: "东-9教"
         },
-        "2018/3/19p": {
-        	subject: "急诊学"
+        "2018/12/9a": {
+        	subject: "人文医学职业技能培训课程D班(自)",
+            detail: "东-9教"
         },
-        "2018/3/20p": {
-        	subject: "急诊学"
+        "2018/12/16a": {
+        	subject: "人文医学职业技能培训课程D班",
+            detail: "东-9教"
         },
-        "2018/3/21a": {
-            subject: "神经病学"
+        "2018/12/23a": {
+        	subject: "人文医学职业技能培训课程D班(自)",
+            detail: "东-9教"
         },
-        "2018/3/21p": {
-        	subject: "皮肤病学"
+        "2018/12/30a": {
+        	subject: "人文医学职业技能培训课程D班",
+            detail: "东-9教"
         },
-        "2018/3/22a": {
-        	subject: "中西医结合骨伤",
-            detail: "颈椎"
+        "2018/1/6a": {
+        	subject: "人文医学职业技能培训课程D班(自)",
+            detail: "东-9教"
         },
-        "2018/3/22p": {
-        	subject: "神经病学",
-        	detail: "张维"
+        "2018/1/13a": {
+        	subject: "人文医学职业技能培训课程D班(自)",
+            detail: "东-9教"
         },
-        "2018/3/26p": {
-        	subject: "急诊学"
-        },
-        "2018/3/27p": {
-        	subject: "急诊学"
-        },
-        "2018/3/28p": {
-        	subject: "皮肤病学"
-        },
-        "2018/3/29a": {
-        	subject: "中西医结合骨伤",
-            detail: "创伤"
-        },
-        "2018/3/29p": {
-        	subject: "神经病学",
-        	detail: "彭唯娜"
-        },
-        "2018/4/2p": {
-        	subject: "急诊学"
-        },
-        "2018/4/3p": {
-        	subject: "急诊学"
-        },
-        "2018/4/4p": {
-        	subject: "皮肤病学"
-        },
-        "2018/4/9p": {
-        	subject: "急诊学"
-        },
-        "2018/4/10p": {
-        	subject: "神经病学",
-        	detail: "考试"
-        },
-        "2018/4/11p": {
-        	subject: "皮肤病学"
-        },
-        "2018/4/12p": {
-        	subject: "中西医结合骨伤",
-        	detail: "考试"
-        },
-        "2018/4/16p": {
-        	subject: "急诊学"
-        },
-        "2018/4/17p": {
-        	subject: "急诊学"
-        },
-        "2018/4/18p": {
-        	subject: "皮肤病学"
-        },
-        "2018/4/23p": {
-        	subject: "急诊学"
-        },
-        "2018/4/24p": {
-        	subject: "急诊学"
-        },
-        "2018/4/25p": {
-        	subject: "皮肤病学"
-        },
-        "2018/5/9p": {
-        	subject: "皮肤病学",
-        	detail: "考试"
-        },
-        "2018/5/16a": {
-        	subject: "急诊学",
-        	detail: "考试"
+        "2018/1/20a": {
+        	subject: "人文医学职业技能培训课程D班(自)",
+            detail: "东-9教"
         }
     }
 };
